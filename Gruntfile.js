5f9bb1e973bdbd4c168514185192eb3ae9042f6e
Ederson Andrade <ederson1411@gmail.com>
2025-06-19 14:20:07 -0300

    modificação

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,51 +1,70 @@
 module.exports = function(grunt) {
-    grunt.initConfig({
-      pkg: grunt.file.readJSON('package.json'),
-      less: {
-        desenvolvimento: {
-          files: {
-            'dist/styles.css': 'src/styles/main.less'
-          }
-        }
-      },
-      cssmin: {
-        alvo: {
-          files: {
-            'dist/styles.min.css': ['dist/styles.css']
-          }
-        }
-      },
-      uglify: {
-        build: {
-          files: {
-            'dist/app.min.js': ['src/scripts/app.js']
-          }
-        }
-      },
-      htmlmin: {
-        dist: {
-          options: {
-            removeComments: true,
-            collapseWhitespace: true
-          },
-          files: {
-            'dist/index.html': 'src/index.html'
-          }
-        }
-      },
-      watch: {
-        files: ['src/**/*.less'],
-        tasks: ['less', 'cssmin']
-      }
-    });
-  
-    grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-htmlmin');
-    grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.registerTask('watch' ['watch']);
-    grunt.registerTask('default', ['less', 'cssmin', 'uglify', 'htmlmin']);
-  };
-  
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    less: {
+      desenvolvimento: {
+        files: {
+          'dev/styles.css': 'src/styles/main.less'
+        }
+      }
+    },
+
+    copy: {
+      html: {
+        files: [
+          {
+            expand: true,
+            cwd: 'src/',
+            src: ['index.html'],
+            dest: 'dev/'
+          }
+        ]
+      }
+    },
+
+    cssmin: {
+      alvo: {
+        files: {
+          'dist/styles.min.css': ['dev/styles.css']
+        }
+      }
+    },
+
+    uglify: {
+      build: {
+        files: {
+          'dist/app.min.js': ['src/scripts/app.js']
+        }
+      }
+    },
+
+    htmlmin: {
+      dist: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {
+          'dist/index.html': 'dev/index.html'
+        }
+      }
+    },
+
+    watch: {
+      files: ['src/**/*.less', 'src/**/*.html'],
+      tasks: ['less', 'copy', 'cssmin', 'htmlmin']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['less', 'copy', 'cssmin', 'uglify', 'htmlmin']);
+  grunt.registerTask('watchTask', ['watch']);
+};
